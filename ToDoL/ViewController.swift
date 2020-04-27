c3cd716a896188009ec8c56ec19d6c27f4e26057
QL31 <qinglian.li1070@gmail.com>
2020-04-28 00:35:04 +0200

    Todo list complete with datasource and delegate

---
+++ ToDoL/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  ToDoL
+//
+//  Created by li qinglian on 27/04/2020.
+//  Copyright © 2020 li qinglian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    
+    let itemArray=["Find toillet paper","Buy Spaguetti","Buy water"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell=tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text=itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    //MARK-TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //print(indexPath.row)
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+
+}
+
